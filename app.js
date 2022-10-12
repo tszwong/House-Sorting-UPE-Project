e547bce7ffcdeb4fdab757134ded95904d066261
Tsz Kit Wong <wongt@bu.edu>
2022-10-12 13:39:12 -0400

    Add files via upload
    initital stage, created base html and css template, created js file

---
+++ app.js
@@ -0,0 +1,6 @@
+let numHouses = document.getElementById("numHouses");
+let nameHouses = document.getElementById("nameHouses");
+const createTeamsButton = document.getElementById("createTeamsButton");
+let output = document.getElementById("output");
+
+
