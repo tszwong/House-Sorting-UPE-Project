cbeb643bbd1b3086716b03194b869084e16ec54a
Tsz Kit Wong <wongt@bu.edu>
2022-10-14 16:15:17 -0400

    Update app.js
    added more variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let numHouses = document.getElementById("numHouses");
-let nameHouses = document.getElementById("nameHouses");
+const numHouses = document.getElementById("numHouses");
+const nameHouses = document.getElementById("nameHouses");
 const createTeamsButton = document.getElementById("createTeamsButton");
-let output = document.getElementById("output");
-
-
+const output = document.getElementById("output");
+const reset = document.getElementById("reset");
+const members = document.querySelector(".members");
+const teams = document.querySelector(".teams");
