a660185209b81f969d7b6046f1fdd61a4a8cf839
Tsz Kit Wong <wongt@bu.edu>
2022-10-14 20:14:50 -0400

    Update app.js
    initial implementation of getInput to get data from textarea box

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const numHouses = document.getElementById("numHouses");
-const nameHouses = document.getElementById("nameHouses");
-const createTeamsButton = document.getElementById("createTeamsButton");
-const output = document.getElementById("output");
-const reset = document.getElementById("reset");
-const members = document.querySelector(".members");
-const teams = document.querySelector(".teams");
+function getInput() {
+    let names = document.getElementById("input").value;
+    let separate = names.split("\n");
+    let name_family_pairs = new Map();
+
+    for (let i = 0; i < separate.length; i++) {
+        alert(separate[i]);
+    }
+}
