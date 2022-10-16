610a903de2858e355d72df619caacebefebd36b7
Tsz Kit Wong <wongt@bu.edu>
2022-10-15 20:29:22 -0400

    Update app.js
    cleared file

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-function getInput() {
-    let names = document.getElementById("input").value;
-    let separate = names.split("\n");
-    let name_family_pairs = new Map();
 
-    for (let i = 0; i < separate.length; i++) {
-        alert(separate[i]);
-    }
-}
